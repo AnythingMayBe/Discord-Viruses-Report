3679b3497cfa62df4157e80e25edbf5f30b528a3
AnythingMayBe <anythingmaybe@protonmail.com>
2021-12-24 19:24:37 +0100

    Added automatic stats writes feature

--- analyze.py
+++ analyze.py
@@ -2,6 +2,7 @@ import requests
 import json
 
 
+
 class Analyzer:
     def __init__(self, apikey):
         self.apikey = apikey
@@ -18,27 +19,41 @@ class Analyzer:
         }
         
         response = requests.request("GET", url, headers=headers)
-
         return json.loads(response.text)
     
     
 
 if __name__ == "__main__":
-    analyze = Analyzer("your VT API Key here")
+    analyze = Analyzer("Enter your VT API Key here")
     
+    # Read Stats
+    with open('stats.json', 'r') as file:
+        rl = json.loads(file.read())
+        file.close()
 
     
     # Get File Scan
     filescan = analyze.getfilereport(input('Scan ID: '))
-    #print(filescan["data"]["attributes"]["last_analysis_results"]["Gridinsoft"]["category"])
-    for av in filescan["data"]["attributes"]["last_analysis_results"]:
-        if filescan["data"]["attributes"]["last_analysis_results"][av]["category"] == "malicious":
-            print(av + ":" + filescan["data"]["attributes"]["last_analysis_results"][av]["category"])
-            analyze.json[av] += 1
+    if not filescan["data"]["attributes"]["sha256"] in rl["testedHashes"]:
+        #print(filescan["data"]["attributes"]["last_analysis_results"]["Gridinsoft"]["category"])
+        for av in filescan["data"]["attributes"]["last_analysis_results"]:
+            if filescan["data"]["attributes"]["last_analysis_results"][av]["category"] == "malicious":
+                print(av + ":" + filescan["data"]["attributes"]["last_analysis_results"][av]["category"])
+                analyze.json[av] += 1
     
-    # Write info
-    with open('results.json', 'w') as file:
+        # Write info
+        with open('results.json', 'w') as file:
+            
+            file.write(str(analyze.json).replace("'", '"'))
+            file.close()
         
-        file.write(str(analyze.json).replace("'", '"'))
-        file.close()
-    
+        # Writing Stats
+
+        with open('stats.json', 'w') as file:
+            rl["testedSamples"] += 1
+            rl["testedHashes"].append(filescan["data"]["attributes"]["sha256"])
+            file.write(str(rl).replace("'", '"'))
+            file.close()
+    else:
+        print('File already scanned.')
+        exit()
